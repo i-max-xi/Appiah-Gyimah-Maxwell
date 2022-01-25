2b214cfc7d590b3cff5f56ab377f5b567a7062a6
i-max-xi <appiahmax4u@gmail.com>
2022-01-25 09:30:13 +0000

    Fix eslint

--- scripts/main.js
+++ scripts/main.js
@@ -1,21 +1,16 @@
-    "useStrict";
-    
-    // Variables
-    let hamburger = document.querySelector('#menu');
-    let close_btn = document.querySelector('#close_icon');
-    let menu_links = document.querySelectorAll('.menu_link');
-    let menu = document.querySelector('.menu-space');
+"useStrict";
+const hamburger = document.querySelector('#menu');          // Variables
+const closeBtn = document.querySelector('#close_icon');
+const menuLinks = document.querySelectorAll('.menu_link');
+const menu = document.querySelector('.menu-space');
 
-    // Functions
-    let displayMenu = () => {
-        menu.style.display = "block";
-    }
-    let closeMenu = () => {
-        menu.style.display = "none";
-    }
-
-    // Implementations
-    menu_links.forEach(el => el.addEventListener('click', closeMenu));
-    hamburger.addEventListener('click',displayMenu);
-    close_btn.addEventListener('click',closeMenu);
-
+let displayMenu = () => {                                   // Functions
+    menu.style.display = "block";
+};
+let closeMenu = () => {
+    menu.style.display = "none";
+};   
+ 
+menuLinks.forEach(el => el.addEventListener('click', closeMenu)); // Implementations
+hamburger.addEventListener('click', displayMenu);
+closeBtn.addEventListener('click', closeMenu);
