4989f36d037a368a48c20ee677ad271b8340f89a
Maxwell Appiah Gyimah <maxappiahofficial@gmail.com>
2022-03-03 23:16:01 +0000

    fix linters

--- scripts/main.js
+++ scripts/main.js
@@ -35,45 +35,49 @@ closeBtn.addEventListener('click', closeMenu);
 //Scroll to top
 window.addEventListener('scroll', () => {
   var scroll = document.querySelector('.fa-angle-up');
-  if (window.scrollY > 500){
-  scroll.classList.add('scrollActive');
-  }
-  else {
+  if (window.scrollY > 500) {
+    scroll.classList.add('scrollActive');
+  } else {
     scroll.classList.remove('scrollActive');
   }
-})
+});
 
 const scrollToTop = () => {
   window.scrollTo({
-    top: 0 ,
-    behavior : 'smooth'
-  })
-}
+    top: 0,
+    behavior: 'smooth',
+  });
+};
 
 // Pop Up Window
 const popUp = [
   {
-    title: '<h1>Ristorant Confusion</h1>',
-    description: "<p> A Restaurant publicity and order website We take inpiration from the World's best Cuisine to create alipsmacking and satisfying collection<p/>",
-    card: '<img src="./Assets/restaurant.JPG" alt="project card display"/>',
-    technologies: '<ul class="pale-info-container"><li class="pale-info">html</li><li class="pale-info">Css</li><li class="pale-info">JavaScript</li></ul>',
-    action: `<button class="see-project" id="popUpBtn1" type="button" onclick="window.open('https://i-max-xi.github.io/Restaurant-site/')">See Live<span class="fa fa-external-link"></span></button><button class="see-project" id="popUpBtn2" type="button">See Source<span class="fa fa-github"></span></button>`,
+    title: `<h1>Ristorant Confusion</h1>`,
+    description:
+      `<p> A Restaurant publicity and order website We take inpiration from the World's best Cuisine to create alipsmacking and satisfying collection<p/>`,
+    card: `<img src='./Assets/restaurant.JPG' alt='project card display'/>`,
+    technologies:
+      `<ul class='pale-info-container'><li class='pale-info'>html</li><li class='pale-info'>Css</li><li class='pale-info'>JavaScript</li></ul>`,
+    action: `<button class='see-project' id='popUpBtn1' type='button' onclick='window.open('https://i-max-xi.github.io/Restaurant-site/')'>See Live<span class='fa fa-external-link'></span></button><button class='see-project' id='popUpBtn2' type='button'>See Source<span class='fa fa-github'></span></button>`,
   },
 
   {
-    title: "<h1>Men's Fashion</h1>",
-    description: "<p> A clothes advertisment page for classic men's wear<p/>",
-    card: '<img src="./Assets/men_fashion_1.PNG" alt="project card display"/>',
-    technologies: '<ul class="pale-info-container"><li class="pale-info">html</li><li class="pale-info">Css</li><li class="pale-info">JavaScript</li></ul>',
-    action: `<button class="see-project" id="popUpBtn1" type="button" onclick="window.open('https://mensfashion.netlify.app/')">See Live<span class="fa fa-external-link"></span></button><button class="see-project" id="popUpBtn2" type="button">See Source<span class="fa fa-github"></span></button>`,
+    title: `<h1>Men's Fashion</h1>`,
+    description: `<p> A clothes advertisment page for classic men's wear<p/>`,
+    card: `<img src='./Assets/men_fashion_1.PNG' alt='project card display'/>`,
+    technologies:
+      `<ul class='pale-info-container'><li class='pale-info'>html</li><li class='pale-info'>Css</li><li class='pale-info'>JavaScript</li></ul>`,
+    action: `<button class='see-project' id='popUpBtn1' type='button' onclick='window.open('https://mensfashion.netlify.app/')'>See Live<span class='fa fa-external-link'></span></button><button class='see-project' id='popUpBtn2' type='button'>See Source<span class='fa fa-github'></span></button>`,
   },
 
   {
     title: `<h1>Stuff 'n' Random</h1>`,
-    description: '<p> A resource library to provide basic things and skills that come in handy in the digital world<p/>',
-    card: '<img src="./Assets/stuffnrandom.JPG" alt="project card display"/>',
-    technologies: '<ul class="pale-info-container"><li class="pale-info">html</li><li class="pale-info">Css</li><li class="pale-info">JavaScript</li></ul>',
-    action: `<button class="see-project" id="popUpBtn1" type="button" onclick="window.open('https://stuffnrandomofficial.netlify.app/')">See Live<span class="fa fa-external-link"></span></button><button class="see-project" id="popUpBtn2" type="button">See Source<span class="fa fa-github"></span></button>`,
+    description:
+      `<p> A resource library to provide basic things and skills that come in handy in the digital world<p/>`,
+    card: `<img src='./Assets/stuffnrandom.JPG' alt='project card display'/>`,
+    technologies:
+      `<ul class='pale-info-container'><li class='pale-info'>html</li><li class='pale-info'>Css</li><li class='pale-info'>JavaScript</li></ul>`,
+    action: `<button class='see-project' id='popUpBtn1' type='button' onclick='window.open('https://stuffnrandomofficial.netlify.app/')'>See Live<span class='fa fa-external-link'></span></button><button class='see-project' id='popUpBtn2' type='button'>See Source<span class='fa fa-github'></span></button>`,
   },
 ];
 
@@ -82,9 +86,11 @@ const popUpDisplay = () => {
   popUpSpace.style.display = 'block';
   blur.forEach((el) => el.classList.add('active'));
   document.querySelector('#popUptitle').innerHTML = popUp[selectedPopUP].title;
-  document.querySelector('#popUpdescription').innerHTML = popUp[selectedPopUP].description;
+  document.querySelector('#popUpdescription').innerHTML =
+    popUp[selectedPopUP].description;
   document.querySelector('#popUpCard').innerHTML = popUp[selectedPopUP].card;
-  document.querySelector('#technologies').innerHTML = popUp[selectedPopUP].technologies;
+  document.querySelector('#technologies').innerHTML =
+    popUp[selectedPopUP].technologies;
   document.querySelector('#action').innerHTML = popUp[selectedPopUP].action;
 };
 
@@ -135,11 +141,13 @@ formSpace.addEventListener('input', () => {
   localStorage.setItem('collectedInput', JSON.stringify(collectedInput));
 });
 
-const collectedInputRetrive = JSON.parse(localStorage.getItem('collectedInput'));
+const collectedInputRetrive = JSON.parse(
+  localStorage.getItem('collectedInput')
+);
 window.onload = () => {
   if (collectedInputRetrive != null) {
     Username.value = collectedInputRetrive.name;
     Useremail.value = collectedInputRetrive.email;
     Usermessage.value = collectedInputRetrive.message;
   }
-}
+};
